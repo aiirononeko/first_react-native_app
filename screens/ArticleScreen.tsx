845260b645a3b0a56a77359bad51d229edd54ee2
片田凌太 <aiirononeko@gmail.com>
2020-03-29 03:39:14 +0900

    redux導入

--- screens/ArticleScreen.tsx
+++ screens/ArticleScreen.tsx
@@ -1,25 +1,35 @@
 import React from 'react';
 import { StyleSheet, TouchableOpacity, Text, SafeAreaView } from 'react-native';
 import { WebView } from 'react-native-webview';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { addClip, deleteClip } from '../store/actions/user';
+import ClipButtom from '../components/ClipButtom';
 
 export default function AppNavigator({ route }) {
   const { article } = route.params;
   const dispatch = useDispatch();
+  const user = useSelector(state => state.user);
+  const { clips } = user;
+
+  const isCliped = () => {
+    return clips.some(clip => clip.url == article.url );
+  }
+
+  const toggleClip = () => {
+    if (isCliped()) {
+      dispatch(deleteClip({
+        clip: article
+      }))
+    } else {
+      dispatch(addClip({
+        clip: article
+      }))
+    }
+  }
 
   return (
     <SafeAreaView style={styles.container}>
-      <TouchableOpacity onPress={() => { dispatch(addClip({
-        clip: article
-      }))}}>
-        <Text style={{margin: 10, fontSize: 30}}>ADD_CLIP</Text>
-      </TouchableOpacity>
-      <TouchableOpacity onPress={() => { dispatch(deleteClip({
-        clip: article
-      }))}}>
-        <Text style={{margin: 10, fontSize: 30}}>DELETE_CLIP</Text>
-      </TouchableOpacity>
+      <ClipButtom onPress={toggleClip} enabled={isCliped()}/>
       <WebView
         source={{ uri: article.url }}
       />
